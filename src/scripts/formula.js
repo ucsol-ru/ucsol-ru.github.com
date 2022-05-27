a8a04540c2c7756089133a2f46c953f2ab08b080
Artik Zenevich <azenevich91@gmail.com>
2022-05-27 14:38:28 +0300

    complite simple converter

--- src/scripts/formula.js
+++ src/scripts/formula.js
@@ -1,6 +1,13 @@
 window.addEventListener('load', () => {
   const inputHp = document.getElementById('hp-input')
   const inputKwt = document.getElementById('kwt-input')
+  const resetButton = document.getElementById('reset-form')
+
+  resetButton.addEventListener('click', (e) => {
+    e.preventDefault()
+    inputHp.value = ''
+    inputKwt.value = ''
+  })
 
   inputHp.addEventListener('keyup', () => {
     let result = inputHp.value / 1.36
@@ -11,5 +18,5 @@ window.addEventListener('load', () => {
     let result = inputKwt.value / 0.74
     inputHp.value = result.toFixed(2)
   })
-
+  
 })
