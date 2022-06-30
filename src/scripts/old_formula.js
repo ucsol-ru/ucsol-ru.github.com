6cbcd0d89d81fe69d2086206ba1d3451af1224b0
Artik Zenevich <azenevich91@gmail.com>
2022-06-30 12:53:36 +0300

    video convert

---
+++ src/scripts/old_formula.js
@@ -0,0 +1,60 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const inputHp = document.getElementById('hp-input')
+  const inputKwt = document.getElementById('kwt-input')
+  const allInputs = document.querySelectorAll('.convert-full__input')
+  const resetButton = document.getElementById('reset-form')
+
+  const cKwt = 1.3596
+  const cHp = 0.7355
+
+  // formula
+  function calcFunc (aInput, bInput, cf) {
+    const result = aInput.value / cf
+    bInput.value = result.toFixed(2)    
+  }
+
+  // claer inputs func
+  function resetFunc () {
+    allInputs.forEach((input) => {
+      input.value = ''
+      input.ariaPlaceholder = '0'
+    })
+  }
+
+  // remove Null function
+  function rmNull(input) {
+    if (input.value == 0.00) {
+      input.value = ''
+    }
+  }
+
+  inputKwt.addEventListener('keyup', () => {
+    calcFunc(inputKwt, inputHp, cHp)
+  })
+
+  inputHp.addEventListener('keyup', () => {
+    calcFunc(inputHp, inputKwt, cKwt,)
+  })
+
+  // reset handler
+  resetButton.addEventListener('click', (e) => {
+    e.preventDefault()
+    resetFunc()
+  })
+
+  document.addEventListener('keydown', (e) => {
+    if (e.code === 'Enter') {
+      e.preventDefault()
+      resetFunc() 
+    }
+  })
+
+  // focus input handlers
+  inputKwt.addEventListener('focus', ()=> {
+    rmNull(inputKwt)
+  })
+  
+  inputHp.addEventListener('focus', ()=> {
+    rmNull(inputHp)
+  })
+})
